c6c147f6860827d29f8b9f3a1ae822ce98759117
SMART24\User <fds44fs@nate.com>
2025-06-16 13:31:19 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    print("메인 페이지에 접속했습니다.")
+    return {"message" : "Hello World"}
+
+@app.get("/hello/(name)")
+def say_hello(name: str):
+    return {"message" : f"안녕, {name}님!"}
