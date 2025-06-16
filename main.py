83738caa15ec0a74b08a90fc129cf849f299d25f
SMART24\User <fds44fs@nate.com>
2025-06-16 13:37:14 +0900

    first commit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def read_root():
     print("메인 페이지에 접속했습니다.")
     return {"message" : "Hello World"}
 
-@app.get("/hello/(name)")
+@app.get("/hello/{name}")
 def say_hello(name: str):
     return {"message" : f"안녕, {name}님!"}
